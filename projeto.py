701e785d60e926f3b7cd448568a50d29a1545359
GSalves21 <ga4718962@gmail.com>
2026-04-11 15:23:36 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,3 +1,15 @@
+#função para criar o cadastro
+def cadastro():
+    print("Vamos iniciar seu cadastro: ")
+    cadastro_email_usuario = input("Insira seu email: ")
+    cadastro_usuario_senha = input("crie uma senha: ")
+    print("etapa concluida!")
+    cadastro_usuario_nome = input("Digite um nome de usuário: ")
+    email_usuario = cadastro_email_usuario
+    senha_usuario = cadastro_usuario_senha
+    nome_usuario = cadastro_usuario_senha 
+    usuario = [nome_usuario,email_usuario,senha_usuario]
+    
 #menu inicial:
 
 print("Bem vindo a FEI TV!")
@@ -7,7 +19,18 @@ while acessar_menu_principal != 0 and acessar_menu_principal != 1:
     print("O número digitado é invalido!")
     acessar_menu_principal = int(input("Qual ação você quer realizar? ")) # lembrar de pensar em um nome melhor para essa variavél
 
-#definindo se irá pra login ou cadastro:
+
+#se houver o login quais opções de menu será oferecida ao usuário:
 if acessar_menu_principal == 0:
+    print("login realizado com sucesso")#talvez posteriormente eu mova esse print para uma função de login
+    print("1-Buscar,2-curtir,3-Gerenciar listas")
+    opção_menu_usuário = int(input("Digite a opção que você deseja realizar: "))
+    while opção_menu_usuário < 1 or opção_menu_usuário > 3:
+        print("Opção inválida!")
+        print("1-Buscar,2-curtir,3-Gerenciar listas")
+        opção_menu_usuário = int(input("Digite a opção que você deseja realizar: "))
+
+
+
     
 
