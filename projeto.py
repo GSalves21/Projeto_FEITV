b254595973002fe86cfc3c3cca32740c63119833
Gabriel <ga4718962@gmail.com>
2026-04-30 16:11:12 -0300

    iniciei a primeira parte do projeto

--- projeto.py
+++ projeto.py
@@ -1,29 +1,33 @@
 #funções que usarei:
+
 def Cadastrar(Name,Senha):
-    registro = open("Usuários.txt","a")
-    registro.write("Usuário: %s , Senha: %s \n" % (Name,Senha))
+    registro = open("Usuarios.txt","a")
+    registro.write("Usuario: %s , Senha: %s \n" % (Name,Senha))
     registro.close()
 
+#fim da função
+
+#essa função ira fazer uma verificação se o login é válido
 
 def verificar_login(Usuario,senha):
     login = open("Usuarios.txt","r")
     for  validação in login:
-        if validação == ("Usuário: %s , Senha: %s \n" % (Usuario,Senha)):
+        validação = ("Usuario: %s , Senha: %s \n" % (Usuario,senha))
+        if validação == login.readlines():
+            login.close()
             return True
         else:
             print("O usuário digitado não existe no sistema")
         return False
-
-
-
-
+#fim da função
 
 #menu inicial:
 
 print("Bem vindo a FEI TV!")
-print("caso você deseje fazer login,digite 0,se não possui login,digite 1 para se cadastrar: ")
+print("caso você deseje fazer login  digite 1,se não possui login digite 0 para se cadastrar: ")
 
 #definindo se irá pra login ou cadastro:
+
 acessar_menu_principal = int(input("Qual ação você quer realizar? ")) # lembrar de pensar em um nome melhor para essa variavél
 while acessar_menu_principal != 0 and acessar_menu_principal != 1:
     print("O número digitado é invalido!")
@@ -36,14 +40,34 @@ if acessar_menu_principal == 0:
     cadastro = Cadastrar(Nome,Senha)
 
 #fazer login
-elif acessar_menu_principal == 1:
-    usuario = input("Digite seu nome de usuário: ")
-    senha = input("Digite sua senha: ")
-    login = verificar_login(usuario,senha)
+
+else:
+    if acessar_menu_principal == 1:
+        usuario = input("Digite seu nome de usuário: ")
+        senha = input("Digite sua senha: ")
+        login = verificar_login(usuario,senha)
+
+#se a função retornar falso o código pedirá que o usuário insira denovo usuário e senha e depois chamará a função verificar_login novamente para fazer a verificação e caso seja um usuario válido o loop quebrará e proseguiremos
+    
     if login == False:
         while True:
             usuario = input("Digite seu nome de usuário: ")
             senha = input("Digite sua senha: ")
+            login = verificar_login(usuario,senha)
+            if login == True:
+                break
+print("Bem vindo!")
+
+#agora o usuario podera navegar pelas seções que ele escolher
+
+seção = int(input("Qual seção você deseja acessar? (1-buscar 2-curtir 3-gerenciar lista): "))
+
+# if seção == 1:
+
+
+
+
+
 
 
 
