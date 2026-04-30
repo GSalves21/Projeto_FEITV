8743aace6e23b05a1c0f5a2bfee9e2cf181350bb
GSalves21 <ga4718962@gmail.com>
2026-04-30 19:26:05 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#função para criar o cadastro
-def cadastro():
-    print("Vamos iniciar seu cadastro: ")
-    cadastro_email_usuario = input("Insira seu email: ")
-    cadastro_usuario_senha = input("crie uma senha: ")
-    print("etapa concluida!")
-    cadastro_usuario_nome = input("Digite um nome de usuário: ")
-    email_usuario = cadastro_email_usuario
-    senha_usuario = cadastro_usuario_senha
-    nome_usuario = cadastro_usuario_senha 
-    usuario = [nome_usuario,email_usuario,senha_usuario]
-    
-=======
 #funções que usarei:
 
 def Cadastrar(Name,Senha):
@@ -36,7 +22,6 @@ def verificar_login(Usuario,senha):
         return False
 #fim da função
 
->>>>>>> 8bbc70c2382a4314789f76a08c4a24db4b9ddbc1
 #menu inicial:
 
 print("Bem vindo a FEI TV!")
@@ -49,21 +34,6 @@ while acessar_menu_principal != 0 and acessar_menu_principal != 1:
     print("O número digitado é invalido!")
     acessar_menu_principal = int(input("Qual ação você quer realizar? ")) # lembrar de pensar em um nome melhor para essa variavél
 
-<<<<<<< HEAD
-
-#se houver o login quais opções de menu será oferecida ao usuário:
-if acessar_menu_principal == 0:
-    print("login realizado com sucesso")#talvez posteriormente eu mova esse print para uma função de login
-    print("1-Buscar,2-curtir,3-Gerenciar listas")
-    opção_menu_usuário = int(input("Digite a opção que você deseja realizar: "))
-    while opção_menu_usuário < 1 or opção_menu_usuário > 3:
-        print("Opção inválida!")
-        print("1-Buscar,2-curtir,3-Gerenciar listas")
-        opção_menu_usuário = int(input("Digite a opção que você deseja realizar: "))
-
-
-
-=======
 #Fazer um cadastro
 if acessar_menu_principal == 0:
     Nome = input("Cadastre seu nome: ")
@@ -95,13 +65,3 @@ print("Bem vindo!")
 seção = int(input("Qual seção você deseja acessar? (1-buscar 2-curtir 3-gerenciar lista): "))
 
 # if seção == 1:
-
-
-
-
-
-
-    
->>>>>>> 8bbc70c2382a4314789f76a08c4a24db4b9ddbc1
-    
-
