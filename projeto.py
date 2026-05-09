c2b8675f3b1f36557d3e26488e429b3a9e12cc36
GSalves21 <ga4718962@gmail.com>
2026-05-09 12:14:32 -0300

    mudei drasticamente o codigo
    quando terminei de criar a função de busca e faze-la funcionar perfeitamente percebi que se quiser fazer navegabilidade dos menus exemplo buscar um video mas depois eu querer sair da função de busca e ir para outra opção eu teria que fazer uma grande repetição do código então cada menu eu estou transformando em uma função

--- projeto.py
+++ projeto.py
@@ -7,6 +7,13 @@ def Cadastrar(Name,Senha):
 
 #fim da função
 
+#essa função é o menu de cadastro
+def menu_cadastro():
+        Nome = input("Cadastre seu nome: ")
+        Senha = input("escolha uma senha:" )
+        Cadastrar(Nome,Senha) #chamei a função para realizar o cadastro
+        print("cadastro realizado com sucesso!")
+
 #essa função ira fazer uma verificação se o login é válido
 
 def verificar_login(Usuario,senha):
@@ -21,6 +28,20 @@ def verificar_login(Usuario,senha):
         print("O usuário digitado não existe no sistema")
         return False
 #fim da função
+
+#Aqui está a função do menu de login:
+def menu_login():
+    usuario = input("Digite seu nome de usuário:")
+    senha = input("Digite sua senha:")
+    login = verificar_login(usuario,senha)
+    if login == False: #se a função retornar falso o código pedirá que o usuário insira denovo usuário e senha e depois chamará a função verificar_login novamente para fazer a verificação e caso seja um usuario válido o loop quebrará e proseguiremos
+        while True:
+            usuario = input("Digite seu nome de usuário:")
+            senha = input("Digite sua senha:")
+            login = verificar_login(usuario,senha)
+            if login == True:
+                break
+    print("Bem vindo!")
 
 
 #Agora farei uma função pra salvar varios filmes e suas informações em um arquivo no formato txt e já criar o catalogo:
@@ -46,7 +67,7 @@ dicionario = dicionario_catalogo()
 def busca(video):
     for conteudo in dicionario:
         if video.strip().upper() == conteudo.upper().strip() :
-            return print(conteudo,':',dicionario[conteudo])
+            return f'{conteudo},:,{dicionario[conteudo]}'
     
 
     
@@ -68,39 +89,33 @@ while acessar_menu_principal != 0 and acessar_menu_principal != 1:
 
 #Fazer um cadastro
 if acessar_menu_principal == 0:
-    Nome = input("Cadastre seu nome: ")
-    Senha = input("escolha uma senha:" )
-    Cadastrar(Nome,Senha) #chamei a função para realizar o cadastro
-    print("cadastro realizado com sucesso!")
-
-
+    menu_cadastro()
 #fazer login
 
-else:
-    if acessar_menu_principal == 1:
-        usuario = input("Digite seu nome de usuário:")
-        senha = input("Digite sua senha:")
-        login = verificar_login(usuario,senha)
-
-#se a função retornar falso o código pedirá que o usuário insira denovo usuário e senha e depois chamará a função verificar_login novamente para fazer a verificação e caso seja um usuario válido o loop quebrará e proseguiremos
-    
-    if login == False:
-        while True:
-            usuario = input("Digite seu nome de usuário:")
-            senha = input("Digite sua senha:")
-            login = verificar_login(usuario,senha)
-            if login == True:
-                break
-
-print("Bem vindo!")
-
+if acessar_menu_principal == 1:
+    menu_login()
 #agora o usuario podera navegar pelas seções que ele escolher
 
 seção = int(input("Qual seção você deseja acessar? (1-buscar 2-curtir 3-gerenciar lista): "))
 
 if seção == 1:
-    video = input("Digite o nome do filme/série que você deseja acessar:")
-    resultado = busca(video)
+    while True:
+        print('deseja buscar algum video(S-sim,N-nao)')
+        navegar_menu =(input('Qual acao deseja fazer?'))
+        if navegar_menu == "N":
+            break
+        if navegar_menu == 'S':
+            video = input("Digite o nome do filme/série que você deseja acessar:")
+            resultado = busca(video)
+            if resultado == None:
+                print("Video não encontrado!")
+            else:
+                print(resultado)
+        else:
+            print('erro' )
+        
+
+            
     
         
         
